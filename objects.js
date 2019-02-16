8d54ffb64a8d5fa513aa7793f0c4a043deccca9e
L D <tapircode@gmail.com>
2019-02-16 21:57:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,6 @@
 let playlist = {
   "Daft Punk" : "Something About Us",
-  
   "Joy Division" : ["Atrocity Exhibition"],
-  
   "David Bowie" : ["Sound and Vision", "Be My Wife", "Station to Station"]
 }
 
