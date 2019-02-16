e080d1d368eb149d03ebf14d6b26f26c385dcb4e
L D <tapircode@gmail.com>
2019-02-16 22:10:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,8 +10,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  console.log(delete playlist.artistName)
   delete playlist[artistName]
-  console.log(playlist)
   return playlist;
 }
