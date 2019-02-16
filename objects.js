6c0d01183861bf3712cdc4f2542d815073db0c36
L D <tapircode@gmail.com>
2019-02-16 22:03:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 let playlist = {
   "Daft Punk" : "Something About Us",
-  "Joy Division" : ["Atrocity Exhibition"],
-  "David Bowie" : ["Sound and Vision", "Be My Wife", "Station to Station"]
+  "Joy Division" : "Atrocity Exhibition",
+  "David Bowie" : "Sound and Vision"
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
