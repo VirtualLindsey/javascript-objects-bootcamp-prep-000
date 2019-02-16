71131afa2ffd8a548ee5afd6e08e9866c76c01a1
L D <tapircode@gmail.com>
2019-02-16 21:41:07 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,16 @@
+let playlist = {
+  "Daft Punk" : "Something About Us"
+  
+  "Joy Division" : ["Atrocity Exhibition"]
+  
+  "David Bowie" : ["Sound and Vision", "Be My Wife", "Station to Station"]
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  
+  return playlist;
+}
